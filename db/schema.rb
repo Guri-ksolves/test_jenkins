2336f53fa08400682927519abd06d8c6857595b3
gurlal <gurlal.singh@ksolves.com>
2017-09-21 15:17:12 +0530

    webhook integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921085926) do
+ActiveRecord::Schema.define(version: 20170921094557) do
 
   create_table "articals", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20170921085926) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
